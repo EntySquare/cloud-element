4e896218918546683153de6fc294115c0e22345f
guodayang999 <33561649+guodayang999@users.noreply.github.com>
2021-08-13 16:18:46 +0800

    up

--- test/enty_test.go
+++ test/enty_test.go
@@ -9,6 +9,7 @@ import (
 )
 
 func Test1(t *testing.T) {
+	fmt.Println("run test api /params /response")
 	http.HandleFunc("/params", func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
 
@@ -23,18 +24,21 @@ func Test1(t *testing.T) {
 			fmt.Println(err)
 		}
 		fmt.Println(fp)
-		inb, err := ioutil.ReadFile("../file/c2.params")
+		inb, err := ioutil.ReadFile(".././file/c2.params")
 		if err != nil {
-			inb, err = ioutil.ReadFile("../../file/c2.params")
-		}
-		var c2in Commit2Params
-		if err := json.Unmarshal(inb, &c2in); err != nil {
 			fmt.Println(err)
 		}
+		//fmt.Println(inb)
+		//fmt.Println(err)
+		var c2in2 Commit2Params2
+		var c2in Commit2Params
+		if err := json.Unmarshal(inb, &c2in2); err != nil {
+			fmt.Println(err)
+		}
+		c2in.Commit1Out = c2in2.Phase1Out
 		paramsJson, err := json.Marshal(c2in)
 		if err != nil {
 			fmt.Println(err)
-
 		}
 		_, err = w.Write(paramsJson)
 	})
@@ -61,6 +65,9 @@ type FetchParams struct {
 }
 type Commit2Params struct {
 	Commit1Out []byte
+}
+type Commit2Params2 struct {
+	Phase1Out []byte
 }
 type PostResp struct {
 	SectorNum string
